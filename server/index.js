b5c09decf436703a1d29bc9329956561febe3307
dvaa <93278631+Dava-033@users.noreply.github.com>
2024-01-13 13:10:37 +0700

    Client add React.js

--- server/index.js
+++ server/index.js
@@ -4,14 +4,11 @@ import UserRoute from "./routes/UserRoute.js";
 
 const app = express();
 app.use(cors({
-    origin: 'http://localhost:3000', // Sesuaikan dengan origin React Anda
+    origin: 'http://localhost:3001', // Sesuaikan dengan origin React Anda
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
 }));
 
-
-// const app = express();
-// app.use(cors());
 app.use(express.json());
 app.use(UserRoute);
 const HOST = 'znk.h.filess.io';
@@ -19,3 +16,5 @@ const PORT = 3307;
 
 app.listen(PORT, ()=> 
 console.log(`API running at... ${HOST}:${PORT}!`));
+
+export default app;
